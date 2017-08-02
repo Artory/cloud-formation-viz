081dfdb0943087ecc8c1dd64ca20d8fdf6afee6f
Jakub Valenta <jakub.valenta@artory.com>
2017-08-02 13:40:02 +0200

    cfviz: Add YAML support

--- cloud_formation_viz/cfviz.py
+++ cloud_formation_viz/cfviz.py
@@ -5,13 +5,55 @@ import json
 import sys
 from numbers import Number
 
+import yaml
+
+
+def equals_constructor(self, node):
+    value = self.construct_sequence(node)
+    return value.count(value[0]) == len(value)
+
+
+def join_constructor(self, node):
+    sep, items = self.construct_sequence(node)
+    return sep.join(items)
+
+
+def if_constructor(self, node):
+    condition, then, otherwise = self.construct_sequence(node)
+    return then if condition else otherwise
+
+
+def ref_constructor(self, node):
+    value = self.construct_scalar(node)
+    return {'Ref': value}
+
+
+def getatt_constructor(self, node):
+    value = self.construct_scalar(node)
+    return {'Fn::GetAtt': value}
+
+
+yaml.SafeLoader.add_constructor(u'!Equals', equals_constructor)
+yaml.SafeLoader.add_constructor(u'!Ref', ref_constructor)
+yaml.SafeLoader.add_constructor(u'!Join', join_constructor)
+yaml.SafeLoader.add_constructor(u'!If', if_constructor)
+yaml.SafeLoader.add_constructor(u'!GetAtt', getatt_constructor)
+
+
+def load(f):
+    try:
+        return json.load(f)
+    except json.decoder.JSONDecodeError:
+        f.seek(0)
+        return yaml.safe_load(f)
+
 
 def main():
     if sys.argv[1:2] != []:
         with open(sys.argv[1]) as f:
-            template = json.load(f)
+            template = load(f)
     else:
-        template = json.load(sys.stdin)
+        template = load(sys.stdin)
 
     graph, edges = extract_graph(template.get('Description', ''),
                                  template['Resources'])
